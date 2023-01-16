51004efa6b077028d398841634154d57dcdcf5b4
skskdmsdl <skskdmsdl@gmail.com>
2023-01-16 23:20:39 +0900

    git push test

--- App.js
+++ App.js
@@ -1,25 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { red } from 'colorette';
-=======
->>>>>>> fd1623e6ba6ef63dde07bfa9d26bad3ff70d8473
-=======
->>>>>>> fd1623e6ba6ef63dde07bfa9d26bad3ff70d8473
 
 export default function App() {
   return (
     <View style={styles.container}>
-<<<<<<< HEAD
-<<<<<<< HEAD
       <Text style={styles.text}>Hello</Text>
-=======
-      <Text>Hello</Text>
->>>>>>> fd1623e6ba6ef63dde07bfa9d26bad3ff70d8473
-=======
-      <Text>Hello</Text>
->>>>>>> fd1623e6ba6ef63dde07bfa9d26bad3ff70d8473
       <StatusBar style="auto" />
     </View>
   );
@@ -32,14 +17,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-<<<<<<< HEAD
-<<<<<<< HEAD
   text: {
-    fontSize: 24,
+    fontSize: 28,
     color: "red",
   }
-=======
->>>>>>> fd1623e6ba6ef63dde07bfa9d26bad3ff70d8473
-=======
->>>>>>> fd1623e6ba6ef63dde07bfa9d26bad3ff70d8473
 });
