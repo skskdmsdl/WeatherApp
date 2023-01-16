d0fb6030b95faafcf161544d5dca447739b65072
skskdmsdl <skskdmsdl@gmail.com>
2023-01-16 23:09:47 +0900

    react native init

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+<<<<<<< HEAD
 import { red } from 'colorette';
+=======
+>>>>>>> fd1623e6ba6ef63dde07bfa9d26bad3ff70d8473
 
 export default function App() {
   return (
     <View style={styles.container}>
+<<<<<<< HEAD
       <Text style={styles.text}>Hello</Text>
+=======
+      <Text>Hello</Text>
+>>>>>>> fd1623e6ba6ef63dde07bfa9d26bad3ff70d8473
       <StatusBar style="auto" />
     </View>
   );
@@ -18,8 +25,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+<<<<<<< HEAD
   text: {
     fontSize: 24,
     color: "red",
   }
+=======
+>>>>>>> fd1623e6ba6ef63dde07bfa9d26bad3ff70d8473
 });
