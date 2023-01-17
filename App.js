265f0b124c8cba29b8d52beb04c6b6503131bbe7
skskdmsdl <skskdmsdl@gmail.com>
2023-01-17 22:44:35 +0900

    위치 정보 가져오기 - expo-location, https://home.openweathermap.org/ 활용

--- App.js
+++ App.js
@@ -1,17 +1,47 @@
-import { React } from 'react';
+import React, { useState, useEffect} from 'react';
 import { View, Text, Dimensions, StyleSheet, ScrollView } from 'react-native';
+
+import * as Location from 'expo-location';
 
 // const { height, width } = Dimensions.get("window");
 const { width:SCREEN_WIDTH } = Dimensions.get("window");
 
+const API_KEY = "de0decc5434a828b3c00ea32467a6148";
+
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+
+  const getWeather = async() => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if(!granted){
+      setOk(false);
+    }
+
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].street);
+    const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}`);
+    const json = await response.json();
+    console.log(json);
+  };
+  useEffect(() => {
+    getWeather();
+  }, [])
+
   return (
     // views는 기본적으로 Flex Container임
     // RN의 Flex Direction의 기본값은 Column
     // RN의 사이즈를 줄때는 반응형을 생각해야함(비율을 생각하기)
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView 
         pagingEnabled
