5b1d6610fb8d30c68a813db9d3afe1a546dabc90
skskdmsdl <skskdmsdl@gmail.com>
2023-01-17 21:45:38 +0900

    RN component 활용 및 스타일 변경

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import { React } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Dimensions, StyleSheet, ScrollView } from 'react-native';
+
+// const { height, width } = Dimensions.get("window");
+const { width:SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
   return (
@@ -10,12 +13,29 @@ export default function App() {
       <View style={styles.city}>
         <Text style={styles.cityName}>Seoul</Text>
       </View>
-      <View style={styles.weather}>
+      <ScrollView 
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentontainerstyle={styles.weather}
+      >
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.description}>Sunny</Text>
         </View>
-      </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -31,14 +51,13 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   cityName: {
-    fontSize: 65,
+    fontSize: 58,
     fontWeight: "500",
   },
   weather: {
-    flex: 3,
   },
   day: {
-    flex: 1,
+    width: SCREEN_WIDTH,
     alignItems: "center",
   },
   temp: {
