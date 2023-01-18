b32c37285c79fbf319c36f3462f820bb40fdebbe
skskdmsdl <skskdmsdl@gmail.com>
2023-01-18 21:02:20 +0900

    날씨 api 받아와서 화면에 보여주기, 아이콘도 추가

--- App.js
+++ App.js
@@ -9,10 +9,17 @@ const { width:SCREEN_WIDTH } = Dimensions.get("window");
 const API_KEY = "de0decc5434a828b3c00ea32467a6148";
 
 const icons = {
-  "Clouds": "cloudy"
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Rain: "rains",
+  Drizzle: "rain",
+  Thunderstorm: "lightning",
 }
 
 export default function App() {
+
   const [city, setCity] = useState("Loading...");
   const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
@@ -31,10 +38,10 @@ export default function App() {
       { useGoogleMaps: false }
     );
     setCity(location[0].street);
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
-    // const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
+    // const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
     const json = await response.json();
-    setDays(json.daily);
+    setDays([json]);
   };
   useEffect(() => {
     getWeather();
@@ -55,7 +62,7 @@ export default function App() {
         contentontainerstyle={styles.weather}
       >
         {days?.length === 0 ? (
-          <View style={styles.day}>
+          <View style={{...styles.day, alignItems: "center" }}>
             <ActivityIndicator 
               color="white"
               style={{ marginTop: 10 }}
@@ -73,17 +80,11 @@ export default function App() {
                   justifyContent: "space-between",
                 }}>
                 <Text style={styles.temp}>
-                  {parseFloat(day.temp.day).toFixed(1)}
+                  {parseFloat(day.main.temp).toFixed(1)}
                 </Text>
-                <Fontisto name="cloudy" size={68} color="white" />
+                <Fontisto style={styles.icon} name={icons[day.weather[0].main]} size={68} color="white" />
               </View>
-              
-              <Text style={styles.temp}>
-                {parseFloat(day.temp.day).toFixed(1)}
-              </Text>
-              <Text style={styles.temp}>
-                {parseFloat(day.temp.day).toFixed(1)}
-              </Text>
+
               <Text style={styles.description}>{day.weather[0].main}</Text>
               <Text style={styles.tinyText}>{day.weather[0].description}</Text>
             </View>
@@ -112,11 +113,12 @@ const styles = StyleSheet.create({
   },
   day: {
     width: SCREEN_WIDTH,
-    alignItems: "center",
+    // alignItems: "center",
   },
   temp: {
-    marginTop: 50,
-    fontSize: 168,
+    marginTop: -30,
+    fontWeight: "600",
+    fontSize: 108,
   },
   description: {
     marginTop: -30,
@@ -124,5 +126,9 @@ const styles = StyleSheet.create({
   },
   tinyText: {
     fontSize: 20,
+  },
+  icon: {
+    marginTop: -30,
+    marginRight: 50,
   },
 });
